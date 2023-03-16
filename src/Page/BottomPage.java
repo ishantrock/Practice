f4bd474fbb55c485b6c827ebc55a86828c4e71c8
Ishant <ishantchawla03@gmail.com>
2023-03-16 22:25:31 +0530

    First Commit

---
+++ src/Page/BottomPage.java
@@ -0,0 +1,10 @@
+package Page;
+
+public class BottomPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Page/TopPage.java
@@ -0,0 +1,10 @@
+package Page;
+
+public class TopPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
